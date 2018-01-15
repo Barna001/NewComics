86b922306e2a1f7dc4a93c5656aeffca937b2b26
Lenti Fruzsi <lentifruzsi@codingsans.com>
2018-01-15 14:13:30 +0100

    feat(*): add triggering of plugin due to page

--- popup.js
+++ popup.js
@@ -124,3 +124,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'readcomiconline.to', schemes: ['http', 'https'] }
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});

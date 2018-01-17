c7929a2c0b3a78c7397c0e12db7d991ba2ae543c
Burom Barna <burombarna@gmail.com>
2018-01-17 12:34:00 +0100

    refactor(popup.js): remove console log and reorder

--- popup.js
+++ popup.js
@@ -9,14 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function init() {
   const comics = chrome.storage.sync.get(null, comics => {
-    console.log('COMICS', comics);
     container = document.getElementsByClassName('container')[0];
     Object.keys(comics).forEach(id => {
       const comic = comics[id];
       container.appendChild(createNewComic(id, comic.url, comic.imageSrc, comic.title));
     });
-    handleDelete();
     handleNavigation();
+    handleDelete();
   });
 }
 
@@ -30,6 +29,17 @@ function handleAdd() {
       saveNewComic(newComic.id, url, imageSrc, title);
     });
   }
+}
+
+function createNewComic(id, url, imageSrc, title) {
+  const wrapper = createWrapper(id);
+  const image = createImage(imageSrc);
+  const link = createLink(url, title);
+  const trash = createTrash();
+  wrapper.appendChild(image);
+  wrapper.appendChild(link);
+  wrapper.appendChild(trash);
+  return wrapper;
 }
 
 function handleNavigation() {
@@ -90,17 +100,6 @@ function saveNewComic(id, url, imageSrc, title) {
   });
 }
 
-function createNewComic(id, url, imageSrc, title) {
-  const wrapper = createWrapper(id);
-  const image = createImage(imageSrc);
-  const link = createLink(url, title);
-  const trash = createTrash();
-  wrapper.appendChild(image);
-  wrapper.appendChild(link);
-  wrapper.appendChild(trash);
-  return wrapper;
-}
-
 function createWrapper(id) { 
   const wrapper = document.createElement('div');
   wrapper.classList.add('wrapper');
@@ -125,52 +124,4 @@ function createTrash() {
   const trash = document.createElement('span');
   trash.classList.add('fa', 'fa-trash');
   return trash;
-}
-
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor=' + color + ';';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument 'tabId', the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
 }
